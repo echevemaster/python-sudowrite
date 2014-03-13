66afcc2be1668736726d51956d3cd7276474af7d
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2014-03-13 17:51:55 -0430

    Specifyng status codes

---
+++ python-sudowrite/status_codes.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 echevemaster.org
+# Eduardo Echeverria - <echevemaster@gmail.com>
+
+_status_codes = {
+    100: 'Error #00: Must be specified atleast an user'
+}
