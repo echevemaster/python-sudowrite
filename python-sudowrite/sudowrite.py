2b758242c3070d99a0b2692b5e98ce4b8a315b33
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2014-03-13 21:28:49 -0430

    First attempt to adduser method, first with a fake sudoers file

--- python-sudowrite/sudowrite.py
+++ python-sudowrite/sudowrite.py
@@ -19,6 +19,7 @@ Options:
 from __future__ import unicode_literals, print_function
 import os
 from docopt import docopt
+from functools import wraps
 from status_codes import _status_codes as _msg
 
 __version__ = '0.1.0'
@@ -31,9 +32,11 @@ def useradd(args):
     Add an user to sudoers file
     '''
     username = args.get('<user>')
-
+    cmd = username + '  ALL=(ALL)   ALL\n'
     if username:
-        raise SystemExit(_msg.get(100))
+        with open('sudoers', 'a') as f:
+            f.write(cmd)
+        print(_msg.get(300))
 
 
 def main():
